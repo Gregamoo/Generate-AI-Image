0e7f8cf3aa679fe57417361daab02a4d368cd53a
Greg Carter <gregamonintendo@gmail.com>
2025-06-25 16:29:23 +0100

    Fixed bug with API for user login

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,4 +1,4 @@
-import userModel from "../models/usermodel'js";
+import userModel from "../models/usermodel.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
@@ -35,20 +35,21 @@ const loginUser = async (req, res) => {
     const { email, password } = req.body;
     const user = await userModel.findOne({ email });
     if (!user) {
-      return res({ success: false, message: "User does not exist" });
+      return res.json({ success: false, message: "User does not exist" });
     }
 
-    const isMatching = await bcrypt.compare;
-    password, user.password;
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (isMatch) {
       const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
       res.json({ success: true, token, user: { name: user.name } });
     } else {
-      return res({ success: false, message: "Invalid credentials" });
+      return res.json({ success: false, message: "Invalid credentials" });
     }
   } catch (error) {
     console.log(error);
     res.json({ success: false, message: error.message });
   }
 };
+
+export { registerUser, loginUser };
